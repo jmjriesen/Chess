1d217b8dd41a65f6f64410f88be362b4c4311dd8
jmriesen <jmriesen@iastate.edu>
2020-05-10 15:49:32 -0500

    added action method for the board(I would have called it move but that is reserved word). reformatted the board set up code so it was cleaner.

--- src/lib.rs
+++ src/lib.rs
@@ -24,37 +24,35 @@ impl <'b> Board <'b>{
 
         let mut t = Board{
             grid: Vec::new(),
-                player_one:player_one,
+            player_one:player_one,
             player_two:player_two,
         };
-
+        // set up the 2d vector.
         for x in 0..8{
             t.grid.push(Vec::new());
             for _ in 0..8{
                 t.grid[x].push(None);
             }
         }
+        //placing the pawns
         for x in 0..8{
-            t.grid[1][x] = Some(Box::new(pices::Pawn::new(player_one)));
-            t.grid[6][x] = Some(Box::new(pices::Pawn::new(player_two)));
+            for (player,row) in [(player_one,1),(player_two,6)].iter(){
+                t.grid[*row][x] = Some(Box::new(pices::Pawn::new(player)));
+            }
         }
+        //placing symmetric Pieces.
+        for (player,row) in [(player_one,0),(player_two,7)].iter(){
 
-        t.grid[0][0] = Some(Box::new(pices::Rook::new(player_one)));
-        t.grid[7][0] = Some(Box::new(pices::Rook::new(player_two)));
-        t.grid[0][7] = Some(Box::new(pices::Rook::new(player_one)));
-        t.grid[7][7] = Some(Box::new(pices::Rook::new(player_two)));
+            t.grid[*row][0] = Some(Box::new(pices::Rook::new(player)));
+            t.grid[*row][7] = Some(Box::new(pices::Rook::new(player)));
 
-        t.grid[0][1] = Some(Box::new(pices::Knight::new(player_one)));
-        t.grid[7][1] = Some(Box::new(pices::Knight::new(player_two)));
-        t.grid[0][6] = Some(Box::new(pices::Knight::new(player_one)));
-        t.grid[7][6] = Some(Box::new(pices::Knight::new(player_two)));
+            t.grid[*row][1] = Some(Box::new(pices::Knight::new(player)));
+            t.grid[*row][6] = Some(Box::new(pices::Knight::new(player)));
 
-
-        t.grid[0][2] = Some(Box::new(pices::Bishops::new(player_one)));
-        t.grid[7][2] = Some(Box::new(pices::Bishops::new(player_two)));
-        t.grid[0][5] = Some(Box::new(pices::Bishops::new(player_one)));
-        t.grid[7][5] = Some(Box::new(pices::Bishops::new(player_two)));
-
+            t.grid[*row][2] = Some(Box::new(pices::Bishops::new(player)));
+            t.grid[*row][5] = Some(Box::new(pices::Bishops::new(player)));
+        }
+        //placing asymmetric Pieces.
         t.grid[0][3] = Some(Box::new(pices::Queen::new(player_one)));
         t.grid[7][3] = Some(Box::new(pices::Queen::new(player_two)));
         t.grid[0][4] = Some(Box::new(pices::King::new(player_one)));
@@ -62,6 +60,10 @@ impl <'b> Board <'b>{
 
         t
     }
-
+    pub fn action(& mut self,(x_from,y_from):(usize,usize),(x_to,y_to):(usize,usize)){
+        let mut temp = None;
+        std::mem::swap(&mut self.grid[x_from][y_from], &mut temp);
+        std::mem::swap(&mut self.grid[x_to][y_to], &mut temp);
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use chess::terminal::Print;
 fn main() {
     let jacob = Player::new("Jacob".to_string(),"red".to_string());
     let tony = Player::new("tony".to_string(),"blue".to_string());
-    let board = Board::new(&jacob,&tony);
+    let mut board = Board::new(&jacob,&tony);
+    board.action((0,0),(7,7));
     let _ = board.to_string();
 }
 
